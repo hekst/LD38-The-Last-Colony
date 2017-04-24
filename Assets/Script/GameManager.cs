e032c0dff90ffb92bb89bea302a0382974c8960d
codenum273 <codenum273@gmail.com>
2017-04-23 20:22:00 -0700

    Added a concept of days passed. Added artwork for number panels. Added script to manage those number sprites.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public PanelNumberControl dayDisplay;
+	float dayLengthInSec = 10.0f;
+	int daysPassed = 0;
+	// Use this for initialization
+
+	void Start () {
+		InitializeDays ();
+		StartCoroutine (CountDays ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitializeDays () {
+		dayLengthInSec = 2.0f;
+		daysPassed = 0;
+	}
+	void PassADay () {
+		daysPassed++;
+		dayDisplay.SetPanelNumber (daysPassed);
+	}
+	IEnumerator CountDays () {
+		yield return new WaitForSeconds (dayLengthInSec);
+		PassADay ();
+
+		StartCoroutine (CountDays ());
+	}
+}
---
+++ Assets/Script/PanelNumberControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelNumberControl : MonoBehaviour {
+
+	// Assumes 3 digits.
+	public SpriteRenderer [] digits;
+	public Sprite[] panelNumbers;
+	public Sprite panelNumberOff;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < digits.Length; i++) {
+			digits [i].sprite = panelNumbers[0];
+		}
+	}
+	
+
+	public void SetPanelNumber (int num) {
+		int digit_1;
+		int digit_10;
+		int digit_100;
+
+		if (num > 999) {
+			digit_1 = 9;
+			digit_10 = 9;
+			digit_100 = 9;
+		} else {
+			digit_1 = num % 10;
+			digit_10	= (num % 100) / 10;
+			digit_100	= (num % 1000) / 100;
+		}
+
+		//Debug.Log (digit_100.ToString () + " " + digit_10.ToString () + " " + digit_1.ToString ());
+		digits [0].sprite = panelNumbers [digit_1];
+		digits [1].sprite = panelNumbers [digit_10];
+		digits [2].sprite = panelNumbers [digit_100];
+
+	}
+
+}
