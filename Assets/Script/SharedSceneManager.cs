ffd1f58216767da92788d2896ec67b0f0bd602d1
codenum273 <codenum273@gmail.com>
2017-04-23 23:52:09 -0700

    Added artwork for Exit To Main Menu and its button on Game Scene's control panel.

---
+++ Assets/Script/SharedSceneManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SharedSceneManager : MonoBehaviour {
+
+	/// <summary>
+	/// Scene Manager will never be destroyed. Will be shared among scenes.
+	/// If information should be passed on to another scene, this will be used to carry it around.
+	/// </summary>
+
+
+	public static SharedSceneManager manager;
+
+	void Awake () {
+		if (manager == null) {
+			manager = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void LoadMainMenu () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void LoadGameScene () {
+		SceneManager.LoadScene ("MainScene");
+
+
+	}
+
+}
